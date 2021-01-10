d28bbd8df8575eb4d329c6835899508aff2cbe61
Joshua Hart <joshua.hart@protonmail.com>
2021-01-10 12:01:01 +0000

    reformat landing page links

--- front-end/src/Landing.js
+++ front-end/src/Landing.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import './landing.css'
 
 const AnimatedDiv = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
-  padding: 7rem 3rem;
+  width: 100%;
+  height: 40rem;
   font-family: 'Roboto', sans-serif;
   font-size: 2rem;
   text-align: justify;
@@ -17,9 +18,10 @@ const H2 = styled.h2`
   letter-spacing: 3px;
   font-weight: 500;
   text-align: center;
+  margin-top: 10rem;
 `
 
-const Landing = ({ setPage, navigate }) => {
+const Landing = (props) => {
   return (
     <div
       style={{
@@ -29,21 +31,23 @@ const Landing = ({ setPage, navigate }) => {
         height: '80%',
       }}
     >
-      <a style={{ textDecoration: 'none' }} href='/developer' onClick={navigate}>
+      <Link to={'/developer'} style={{ textDecoration: 'none', width: '50%' }}>
         <AnimatedDiv className='developer-panel'>
           <H2>Developers</H2>
-          <div style={{ fontWeight: 300 }}>
+          <div style={{ fontWeight: 300, padding: '3rem' }}>
             Make your life easy, Set up a custom ICO from a variety of ICO styles and open your
             project up to investment
           </div>
         </AnimatedDiv>
-      </a>
-      <AnimatedDiv className='investor-panel'>
-        <H2>Investors</H2>
-        <div style={{ fontWeight: 300 }}>
-          Be a pioneer, Invest in ICOs of projects listed on this site.
-        </div>
-      </AnimatedDiv>
+      </Link>
+      <Link to={'/investor'} style={{ textDecoration: 'none', width: '50%' }}>
+        <AnimatedDiv className='investor-panel'>
+          <H2>Investors</H2>
+          <div style={{ fontWeight: 300, padding: '3rem' }}>
+            Be a pioneer, Invest in ICOs of projects listed on this site.
+          </div>
+        </AnimatedDiv>
+      </Link>
     </div>
   )
 }
