7d858a3c2ae59880e1f2bd53698252bff62f458d
Joshua Hart <joshua.hart@protonmail.com>
2021-01-15 22:36:14 +0000

    create investors index header

--- front-end/src/developers/DeveloperIndex.js
+++ front-end/src/developers/DeveloperIndex.js
@@ -5,10 +5,31 @@ import NavBar from '../NavBar'
 import developersImage from '../assets/developersAndFounders.svg'
 
 const H2 = styled.h2`
-  letter-spacing: 3px;
-  font-weight: 300;
+  font-family: 'Questrial', sans-serif;
+  font-weight: 400;
   text-align: left;
   font-size: 3rem;
+  display: inline-block;
+`
+
+const LaunchButton = styled.button`
+  font-family: 'Questrial', sans-serif;
+  font-weight: 400;
+  font-size: 1.5rem;
+  background: #ffffff;
+  border: 2px solid #4e3fce;
+  box-sizing: border-box;
+  border-radius: 2rem;
+  width: 25rem;
+  height: 5rem;
+  margin-top: 1.5rem;
+`
+
+const HeaderDiv = styled.div`
+  border-bottom: 1px solid #4e3fce;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 2%;
 `
 
 const DeveloperIndex = (props) => {
@@ -18,8 +39,13 @@ const DeveloperIndex = (props) => {
         imgSource={developersImage}
         titleText={'Developers - Create and Manage your project launches'}
       />
-      <H2>My ICOs:</H2>
-      <SetUpIcoCard />
+      <main style={{ padding: '0 10%' }}>
+        <HeaderDiv>
+          <H2>My Launches</H2>
+          <LaunchButton>Launch a New Project</LaunchButton>
+        </HeaderDiv>
+        <SetUpIcoCard />
+      </main>
     </div>
   )
 }
