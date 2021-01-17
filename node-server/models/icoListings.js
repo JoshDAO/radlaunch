d3ed2bd86f350b27e617471398d23b5363dacf2c
Joshua Hart <joshua.hart@protonmail.com>
2021-01-17 20:19:49 +0000

    set up db schema and createListing endpoint

--- node-server/models/icoListings.js
+++ node-server/models/icoListings.js
@@ -3,15 +3,19 @@ const Schema = mongoose.Schema
 
 const IcoSchema = new Schema(
   {
+    tokenAddress: { type: String, required: true }, // address of the ERC-20 contract
     tokenName: { type: String, required: true },
-    totalSupply: { type: Number, required: true },
-    startPrice: { type: Number, required: true },
-    bondingCurveStyle: { type: String, required: true },
-    startDate: { type: Date },
-    endDate: { type: Date },
-    paymentCurrency: { type: String },
-    fundWallet: { type: String },
+    tokenSymbol: { type: String, required: true },
+    totalSupply: { type: Number, required: true }, // total number of token existing on the ERC-20 contract
+    icoAddress: { type: String, required: true }, // address of contract created by the factory
+    icoTokenSupply: { type: Number, required: true }, // number of tokens made available on the ICO. Must be <= totalSupply
+    launchType: { type: String, required: true },
+    owner: { type: String, required: true }, //project owner's wallet
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    minimumThreshold: { type: Number, required: true }, // given in ETH
     projectDescription: { type: String },
+    etherscanLink: { type: String, required: true }, //'https://etherscan.io/address/${icoAddress}'
   },
   { timestamps: true },
 )
--- node-server/server.js
+++ node-server/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-///const userRouter = require('./routes/user-router')
+const router = require('./routes/listingsRouter')
 
-//const db = require("./db/connection");
+const db = require('./db/connection')
 
 const app = express()
 
@@ -12,11 +12,18 @@ app.use(bodyParser.json())
 app.use(cors())
 app.options('/api/*', cors())
 
-//db.on("error", console.error.bind(console, "MongoDB connection error:"));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!!!!!')
 })
+
+app.use('/api', router)
 
 const apiPort = 8000
 
